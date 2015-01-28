a07fd95e2d59b8b0f8574aafb2f563eafd2ddad7
shycherry <vinpowful@free.fr>
2015-01-29 00:42:51 +0100

    wip

--- index.js
+++ index.js
@@ -1,70 +1,220 @@
 var xor = require('bitwise-xor');
 var mtwister = require('mersenne-twister');
+var _twister = new mtwister();
 
 
 function _nop (){};
 
-function reset(){
-  this.twister = new mtwister();
-  this.flags = 0;
-  this.mask = null;
-  this.updateMutator();
-};
-
-function XORActive(){
-  reset.call(this);
+function _randomFillBuffer(ioBuffer){
+  if(!Buffer.isBuffer(ioBuffer)){
+    return;
+  }
+  
+  var bufferSize = ioBuffer.length;
+  while(bufferSize){
+    ioBuffer.writeUInt32BE(_twister.random_int(), _twister.random_int() % bufferSize, true);
+    bufferSize--;
+  }
 }
 
-XORActive.prototype.isStateOK = function() {
-  if(!this.mask)
-    return false;
-  if(this.mutator){
-    if(this.mask.length < this.mutator.length)
-      return false;
-  }
-  return ();
-};
+function XORActive(){
+  this._flags = 0;
+  this._mask = null;
+  this._sendCb = null;
+  this._mutator = null;
+}
 
 XORActive.prototype.setMask = function(iMask) {
-  this.mask = Buffer.isBuffer(iMask)? iMask : new Buffer(iMask);
+  this._mask = Buffer.isBuffer(iMask)? iMask : new Buffer(iMask);
 };
 
-XORActive.prototype.setMutator = function(iMutator){
-  this.mutator = Buffer.isBuffer(iMutator)? iMutator : new Buffer(iMutator);
+XORActive.prototype.setSendCb = function(iSendCb) {
+  this._sendCb = iSendCb;
 };
 
-XORActive.prototype.format = function(iBuffer) {
+XORActive.prototype._getFormatedPacket = function(iNewMutator,
+iPrevMutator, iPayload, iDesiredSize) {
+  var buffers = [];
+  var totalSize = 0;
+
   var flagsBuf = new Buffer(1);
-  flagsBuf.writeInt8(this.flags, 0);
+  var flagsValue = this._flags || 0;
+  flagsBuf.writeInt8(flagsValue, 0);
+  buffers.push(flagsBuf);
+  totalSize += 1;
 
-  var mutatorLengthBuf = new Buffer(2);
+  var mutatorSizeBuf = new Buffer(1);
+  var mutatorSizeValue = Buffer.isBuffer(iNewMutator) ? iNewMutator.length : 0;
+  mutatorSizeBuf.writeUInt8(mutatorSizeValue, 0);
+  buffers.push(mutatorSizeBuf);
+  totalSize += 1;
 
+  if(mutatorSizeValue){
+    buffers.push(iNewMutator);
+    totalSize += mutatorSizeValue;
+  }
 
-  return new Buffer.concat([iBuffer]);
+  var prevMutatorSizeBuf = new Buffer(1);
+  var prevMutatorSizeValue = Buffer.isBuffer(iPrevMutator) ? iPrevMutator.length : 0;
+  prevMutatorSizeBuf.writeUInt8(prevMutatorSizeValue, 0);
+  buffers.push(prevMutatorSizeBuf);
+  totalSize += 1;
+
+  if(prevMutatorSizeValue){
+    buffers.push(iPrevMutator);
+    totalSize += prevMutatorSizeValue;
+  }
+
+  var payloadSizeBuf = new Buffer(2);
+  var payloadSizeValue = Buffer.isBuffer(iPayload) ? iPayload.length : 0;
+  payloadSizeBuf.writeUInt16BE(payloadSizeValue, 0);
+  buffers.push(payloadSizeBuf);
+  totalSize += 2;
+
+  if(payloadSizeValue){
+    buffers.push(iPayload);
+    totalSize += payloadSizeValue;
+  }
+
+  if(iDesiredSize > totalSize){
+    var paddingBuf = new Buffer(iDesiredSize - totalSize);
+    _randomFillBuffer(paddingBuf);
+    buffers.push(paddingBuf);
+  }
+
+  return new Buffer.concat(buffers);
 };
 
-XORActive.prototype.encrypt = function(iBuffer, iCb) {
+XORActive.prototype._getUnPackedFormat = function(iBuffer) {
+  var results = {};
+
+  var offset = 0;
+  var flagsValue = iBuffer.readInt8(offset);
+  offset += 1;
+  results['flags'] = flagsValue;
+
+  var mutatorSizeValue = iBuffer.readInt8(offset);
+  offset += 1;
+  if(mutatorSizeValue > (iBuffer.length - offset)) return null;
+
+  // results['mutator_size'] = mutatorSizeValue;
+  
+  var mutatorBuf = null;
+  if(mutatorSizeValue){
+    mutatorBuf = new Buffer(mutatorSizeValue);
+    iBuffer.copy(mutatorBuf, 0, offset, offset+mutatorSizeValue);
+    offset += mutatorSizeValue;
+  }
+  results['mutator'] = mutatorBuf;
+
+  var prevMutatorSizeValue = iBuffer.readInt8(offset);
+  offset += 1;
+  if(prevMutatorSizeValue > (iBuffer.length - offset)) return null;
+  
+  // results['prev_mutator_size'] = prevMutatorSizeValue;
+
+  var prevMutatorBuf = null;
+  if(prevMutatorSizeValue){
+    prevMutatorBuf = new Buffer(prevMutatorSizeValue);
+    iBuffer.copy(prevMutatorBuf, 0, offset, offset+prevMutatorSizeValue);
+    offset += mutatorSizeValue;
+  }
+  results['prev_mutator'] = prevMutatorBuf;
+
+  var payloadSizeValue = iBuffer.readUInt16BE(offset);
+  offset += 2;
+  if(payloadSizeValue > (iBuffer.length - offset)) return null;
+
+  // results['payload_size'] = payloadSizeValue;
+
+  var payloadBuf = null;
+  if(payloadSizeValue){
+    payloadBuf = new Buffer(payloadSizeValue);
+    iBuffer.copy(payloadBuf, 0, offset, offset+payloadSizeValue);
+    offset += mutatorSizeValue;
+  }
+
+  return results;
+};
+
+XORActive.prototype.regenMutator = function() {
+  var mutatorBuf = (this._mutator)? new Buffer(this._mutator.length) : new Buffer(4);
+  
+  _randomFillBuffer(mutatorBuf);
+  return mutatorBuf;
+};
+
+XORActive.prototype.send = function(iPayload, iCb) {
   iCb = iCb || _nop;
 
-  if(!this.isStateOK()){
-    iCb("internal state is not OK");
+  if(!Buffer.isBuffer(this._mask)){
+    iCb("no mask provided");
+    return;
+  }
+  
+  if(!this._sendCb){
+    iCb("no sendCb provided");
     return;
   }
 
-  iBuffer = new Buffer(iBuffer);
-  if(!iBuffer || (iBuffer.length + this.mutator.length) > this.mask.length){
-    iCb("invalid input buffer");
+  var newMutator = this.regenMutator();
+  var prevMutator = this._mutator;
+
+  var formattedBuf = this._getFormatedPacket(newMutator, prevMutator, iPayload, this._mask.length);
+  if(!Buffer.isBuffer(formattedBuf)){
+    iCb("unknown error");
     return;
   }
 
-  var result = Buffer.concat([this.mutator, iBuffer]);
-  iCb(null, result);
+  if(formattedBuf.length > this._mask.length){
+    iCb("mask is too short, an upgrade is needed");
+    return;
+  }
+
+  var xorBuf = xor(formattedBuf, this._mask);
+
+  var sentCallback = function(err, data){
+    this._mutateMask(newMutator);
+    iCb(err, data);
+  }.bind(this);
+
+  this._sendCb(xorBuf, sentCallback);
 };
 
-XORActive.prototype.updateMutator = function() {
-  this.prev_mutator = this.mutator;
-  this.mutator = this.mutator || new Buffer(4);
-  this.mutator.set(0,this.twister.random_int());
+XORActive.prototype._mutateMask = function(iMutator){
+  this._mutator = iMutator;
+  this._mask.writeInt8(69,0);
+}
+
+XORActive.prototype.recieve = function(iPacket, iCb) {
+
+  if(!Buffer.isBuffer(this._mask)){
+    iCb("no mask provided");
+    return;
+  }
+  
+  var packetBuf = Buffer.isBuffer(iPacket)? iPacket : new Buffer(iPacket);
+  if(packetBuf.length != this._mask.length){
+    iCb("bad packet size");
+    return;
+  }
+
+  var unXorBuf = xor(packetBuf, this._mask);
+  
+  var unpacked = _getUnPackedFormat(unXorBuf);
+  if(!unpacked){
+    iCb('invalid packet format');
+    return;
+  }
+
+  if(unpacked['prev_mutator'] != this._mutator){
+    iCb('previous mutator doesnt match');
+    return;
+  }
+
+  this._mutateMask(unpacked['mutator']);
+
+  iCb(null, unpacked['payload']);
 };
 
 module.exports = XORActive;
--- test/tests.js
+++ test/tests.js
@@ -1,32 +1,75 @@
 var assert = require('assert');
 var XORActive = require('../index.js');
 
-var testMask = "0123456789";
-var testMutator = "MUTA";
-var connexA = new XORActive();
-var connexB = new XORActive();
+var testMask20Bytes = new Buffer([48,49,50,51,52,53,54,55,56,57,48,49,50,51,52,53,54,55,56,57]) // <=> "01234567890123456789"
+var testMask10Bytes =  new Buffer([48,49,50,51,52,53,54,55,56,57]) // <=> "0123456789"
+var testMutator = new Buffer([77,85,84,65]) // <=> "MUTA"
+var testPayload = new Buffer('secret');
+var testNopSendCb = function(data, cb){cb(null, data);};
+var connexA = null;
+var connexB = null;
 
 describe('test creation', function(){
-
-  it('has null mask', function(){
-    assert.equal(connexA.mask, null);
+  before(function(){
+    connexA = new XORActive();
   });
 
-  it('cant encrypt with null mask', function(){
-    connexA.encrypt('secret message', function(err, result){
-      assert.equal(err, "internal state is not OK");
+  it('has null mask', function(){
+    assert.equal(connexA._mask, null);
+  });
+
+  it('has null sendItf', function(){
+    assert.equal(connexA._sendItf, null);
+  });
+});
+
+describe('test bad masks', function(){
+  before(function(){
+    connexA = new XORActive();
+    connexA.setSendCb(testNopSendCb);
+  });
+
+   it('cant encrypt with null mask', function(){
+    connexA.send(testPayload, function(err, result){
+      assert.equal(err, "no mask provided");
       assert.equal(result, null);
     });
   });
 
-  it('can encrypt with a mask', function(){
-    connexA.setMask(testMask);
-    connexA.setMutator(testMutator);
-    connexA.encrypt('secret', function(err, result){
-      assert.equal(err, null);
-      assert.equal(result, new Buffer([2,1]));
+  it('cant encrypt with a mask too short', function(){
+    connexA.setMask(testMask10Bytes);
+    connexA.send(testPayload, function(err, result){
+      assert.equal(err, "mask is too short, an upgrade is needed");
+      assert.equal(result, null);
     });
   });
+});
 
+describe('test bad sendCb', function(){
+  before(function(){
+    connexA = new XORActive();
+    connexA.setMask(testMask20Bytes);
+  });
 
-})
+   it('cant encrypt with null sendCb', function(){
+    connexA.send(function(err, result){
+      assert.equal(err, "no sendCb provided");
+      assert.equal(result, null);
+    });
+  });
+});
+
+describe('test with known mask', function(){
+  before(function(){
+    connexA = new XORActive();
+    connexA.setMask(testMask20Bytes);
+    connexA.setSendCb(testNopSendCb);
+  });
+
+  it('has expected size', function(){
+    connexA.send(testPayload, function(err, result){
+        assert.equal(err, null);
+        assert.equal(result.length, 20);
+    });
+  });  
+});
